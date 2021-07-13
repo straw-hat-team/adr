2060f677a4fab61bf7866baf2a7554a2690f1e1c
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2021-07-13 10:03:04 -0400

    Add emojis to header menu

--- website/src/components/header.tsx
+++ website/src/components/header.tsx
@@ -19,11 +19,19 @@ export function Header() {
   return (
     <header className="mb-8 shadow-sm sticky top-0 z-10 backdrop-filter backdrop-blur-lg backdrop-saturate-150 bg-white dark:bg-gray-800 bg-opacity-80">
       <div className="flex items-center justify-between w-full max-w-screen-lg xl:max-w-screen-xl py-8 px-4 mx-auto text-gray-900 dark:text-gray-100">
-        <nav>
-          <NavLink href="/">Home</NavLink>
-          <NavLink href="/adrs">ADRs</NavLink>
-          <NavLink href="/contributing">Contributing</NavLink>
-          <NavLink href="/faq">FAQ</NavLink>
+        <nav className="flex text-center">
+          <NavLink href="/">
+            <span className="block">🏡</span>Home
+          </NavLink>
+          <NavLink href="/adrs">
+            <span className="block">📝</span>ADRs
+          </NavLink>
+          <NavLink href="/contributing">
+            <span className="block">👩🏻‍💻</span>Contributing
+          </NavLink>
+          <NavLink href="/faq">
+            <span className="block">❓</span> FAQ
+          </NavLink>
         </nav>
         <ThemeToggler />
       </div>
