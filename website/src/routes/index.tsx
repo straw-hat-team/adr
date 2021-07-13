6bbd64867f1ada2df89a4f2cfb4a348750ae14bc
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2021-07-13 10:40:04 -0400

    Fix link

--- website/src/routes/index.tsx
+++ website/src/routes/index.tsx
@@ -1,8 +1,9 @@
-import Link from 'next/link';
-import { COMPANY_NAME, DISCUSSIONS_QA_URL, GENESIS_ADR } from "@/constants";
+import NextLink from 'next/link';
+import { COMPANY_NAME, DISCUSSIONS_QA_URL, GENESIS_ADR } from '@/constants';
 import { ArrowRightIcon } from '@heroicons/react/solid';
-import { ExternalLinkIcon } from "@heroicons/react/solid";
-import { adrFormat } from "@/helpers";
+import { ExternalLinkIcon } from '@heroicons/react/solid';
+import { adrFormat } from '@/helpers';
+import { Link } from '@/components/link';
 
 export function IndexRoute() {
   return (
@@ -17,16 +18,16 @@ export function IndexRoute() {
           {COMPANY_NAME} and the means by which teams discuss and come to consensus on software development guidance.
         </p>
         <div className="flex flex-wrap space-y-4 sm:space-y-0 sm:space-x-4 text-center">
-          <Link href="/adrs" passHref>
+          <NextLink href="/adrs" passHref>
             <a className="w-full sm:w-auto flex-none bg-gray-900 hover:bg-gray-700 text-white dark:bg-gray-200 dark:hover:bg-gray-300 dark:text-gray-700 text-lg leading-6 font-semibold py-6 px-12 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200">
               Explore the ADRs
             </a>
-          </Link>
-          <Link href={`/adrs/${GENESIS_ADR}`} passHref>
+          </NextLink>
+          <NextLink href={`/adrs/${GENESIS_ADR}`} passHref>
             <a className="w-full sm:w-auto flex-none bg-gray-900 hover:bg-gray-700 text-white dark:bg-gray-200 dark:hover:bg-gray-300 dark:text-gray-700 text-lg leading-6 font-semibold py-6 px-12 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200">
               Learn how it works
             </a>
-          </Link>
+          </NextLink>
         </div>
       </header>
       <section className="grid gap-10 grid-cols-1 lg:grid-cols-2">
@@ -35,30 +36,30 @@ export function IndexRoute() {
             Curious about the basics?
           </h2>
           <p>
-            ADRs are a combination of design guidance and a system we use to manage and track that guidance. Learn more
-            about how the ADR works in the first ADR!
+            ADRs are a collection of guidance and a system we use to manage and track that guidance. Learn more about
+            how the ADR works in the first ADR!
           </p>
-          <Link href={`/adrs/${GENESIS_ADR}`} passHref>
-            <a className="inline-flex items-center text-gray-700 dark:text-gray-300 border-2 border-gray-700 hover:border-gray-500 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200">
+          <NextLink href={`/adrs/${GENESIS_ADR}`} passHref>
+            <a className="inline-flex items-center text-gray-700 dark:text-gray-300 border-2 border-gray-700 hover:border-gray-500 text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200">
               READ {adrFormat(GENESIS_ADR)}
               <ArrowRightIcon className="w-text w-text ml-4" role="img" aria-hidden="true" />
             </a>
-          </Link>
+          </NextLink>
         </div>
         <div className="space-y-4">
           <h2 className="text-xl sm:text-3xl leading-none font-extrabold text-black dark:text-white tracking-tight">
             Want to help?
           </h2>
           <p>
-            Interested in helping with ADRs? Contribute by proposing new guidance, commenting on existing ADRs, or
-            fixing typos. All contributions are welcome!
+            Contribute by proposing new guidance, commenting on existing ADRs, or fixing typos. All contributions are
+            welcome!
           </p>
-          <Link href="/adrs" passHref>
-            <a className="inline-flex items-center text-gray-700 dark:text-gray-300 border-2 border-gray-700 hover:border-gray-500 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200">
+          <NextLink href="/adrs" passHref>
+            <a className="inline-flex items-center text-gray-700 dark:text-gray-300 border-2 border-gray-700 hover:border-gray-500 text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200">
               Contribute to the project
               <ArrowRightIcon className="w-text w-text ml-4" role="img" aria-hidden="true" />
             </a>
-          </Link>
+          </NextLink>
         </div>
         <div className="space-y-4">
           <h2 className="text-xl sm:text-3xl leading-none font-extrabold text-black dark:text-white tracking-tight">
@@ -66,12 +67,16 @@ export function IndexRoute() {
           </h2>
           <p>
             Free free to take a look at the&nbsp;
-            <Link href="/faq" passHref>
-              <a>frequently asked questions</a>
+            <Link href="/faq">
+              frequently asked questions
             </Link>
-            . If you don't find an answer there, file an issue on our GitHub repository.
+            . If you don't find an answer there, create a new discussion on our GitHub Discussions.
           </p>
-          <a target="_blank" href={DISCUSSIONS_QA_URL} className="inline-flex items-center text-gray-700 dark:text-gray-300 border-2 border-gray-700 hover:border-gray-500 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200">
+          <a
+            target="_blank"
+            href={DISCUSSIONS_QA_URL}
+            className="inline-flex items-center text-gray-700 dark:text-gray-300 border-2 border-gray-700 hover:border-gray-500 text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200"
+          >
             Ask us on Github
             <ExternalLinkIcon className="w-text w-text ml-4" role="img" aria-hidden="true" />
           </a>
