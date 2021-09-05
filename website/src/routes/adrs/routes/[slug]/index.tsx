6daddef42966d457acd1dc87d121b07434609d07
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2021-09-05 05:24:41 -0400

    chore: remove frontmatter from markdown

--- website/src/routes/adrs/routes/[slug]/index.tsx
+++ website/src/routes/adrs/routes/[slug]/index.tsx
@@ -1,18 +1,34 @@
 import { MDX } from '@/components/mdx';
 import { MdxFile } from '@/helpers/mdx';
-import { Frontmatter } from '@/routes/contributing';
+import { AdrFrontmatter } from '@/routes/adrs/types';
+import { PropsWithChildren } from 'react';
 
 export type RouteParam = {
   slug: string;
 };
 
 export type SlugProps = {
-  post: MdxFile<Frontmatter>;
+  post: MdxFile<AdrFrontmatter>;
 };
+
+function Badge(props: PropsWithChildren<{}>) {
+  return (
+    <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-red-100 text-red-800">
+      {props.children}
+    </span>
+  );
+}
 
 export function Slug(props: SlugProps) {
   return (
     <main className="px-4 max-w-screen-lg xl:max-w-screen-xl mx-auto space-y-10 lg:space-y-20 prose lg:prose-xl">
+      <header>
+        <div className="flex gap-4 items-center">
+          {props.post.frontmatter.tags.map((tag) => (
+            <Badge key={tag}>{tag}</Badge>
+          ))}
+        </div>
+      </header>
       <MDX source={props.post.code} />
     </main>
   );
