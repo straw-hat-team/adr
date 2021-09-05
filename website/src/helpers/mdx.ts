4e91b18a0700b2b5a8e1a4195f395ca51bb906ba
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2021-09-05 02:01:12 -0400

    chore: improve mdx

--- website/src/helpers/mdx.ts
+++ website/src/helpers/mdx.ts
@@ -14,9 +14,12 @@ type MdxFileRoute = {
   routeDir: RouteDirEntry;
 };
 
-export type ReadFileSyncConfig = { atRootDir: string };
+export type ReadFileConfig = { atRootDir: string };
 
-export type ReadMdxFileConfig = Exclude<BundleMDXOptions, 'cwd'> & Partial<ReadFileSyncConfig>;
+export type ReadMdxFileConfig = {
+  mdxOptions?: Exclude<BundleMDXOptions, 'cwd'>;
+  atRootDir?: string;
+};
 
 export type MdxFile<FM = unknown> = {
   code: string;
@@ -44,36 +47,7 @@ function readMdxForRoute(config: ReadMdxFileConfig) {
   };
 }
 
-export function readAllMdxUnderRoute(pathSegment: string, config: ReadMdxFileConfig) {
-  const mdxFiles = readSubRouteOfRoute(pathSegment, { atRootDir: config.atRootDir ?? SRC_DIR })
-    .filter(isMdxRoute)
-    .map(readMdxForRoute(config));
-
-  return Promise.all(mdxFiles);
-}
-
-export async function readMdxFile<FM = unknown>(
-  pathSegment: string,
-  config: ReadMdxFileConfig = {}
-): Promise<MdxFile<FM>> {
-  const { atRootDir = SRC_DIR, ...mdxConfig } = config;
-  const source = readFileSync(pathSegment, {
-    atRootDir: atRootDir,
-  });
-
-  const compiled = await bundleMDX(source, {
-    ...mdxConfig,
-    cwd: SRC_DIR,
-  });
-
-  return {
-    code: compiled.code,
-    frontmatter: compiled.frontmatter as FM,
-    matter: compiled.matter,
-  };
-}
-
-function normalizePath(pathSegment: string, config: ReadFileSyncConfig) {
+function normalizePath(pathSegment: string, config: ReadFileConfig) {
   return pathSegment.replace(/^@/, config.atRootDir);
 }
 
@@ -88,17 +62,47 @@ function withFilePath(routesDir: string) {
   });
 }
 
-export function readSubRouteOfRoute(pathSegment: string, config: ReadFileSyncConfig) {
+function readSubRouteOfRoute(pathSegment: string, config: ReadFileConfig) {
   const routesDir = path.join(normalizePath(pathSegment, config), 'routes');
   return fs.readdirSync(routesDir, { withFileTypes: true }).filter(isDirectory).map(withFilePath(routesDir));
 }
 
-export function readFileSync(pathSegment: string, config: ReadFileSyncConfig) {
-  return fs.readFileSync(normalizePath(pathSegment, config), {
+function readFile(pathSegment: string, config: ReadFileConfig) {
+  return fs.promises.readFile(normalizePath(pathSegment, config), {
     encoding: 'utf8',
   });
 }
 
 export function fileNameFromMdxFileRouteToSlugParams(fileRoute: MdxFileRoute) {
   return { params: { slug: fileRoute.routeDir.dirent.name } };
+}
+
+export function readMdxFilesOfRoute(pathSegment: string, config: ReadMdxFileConfig) {
+  const mdxFiles = readSubRouteOfRoute(pathSegment, { atRootDir: config.atRootDir ?? SRC_DIR })
+    .filter(isMdxRoute)
+    .map(readMdxForRoute(config));
+
+  return Promise.all(mdxFiles);
+}
+
+export async function readMdxFile<FM = unknown>(
+  pathSegment: string,
+  config: ReadMdxFileConfig = {}
+): Promise<MdxFile<FM>> {
+  const source = await readFile(pathSegment, {
+    atRootDir: config.atRootDir ?? SRC_DIR,
+  });
+
+  const compiled = await bundleMDX(source, {
+    ...config.mdxOptions,
+    cwd: SRC_DIR,
+  });
+
+  // TODO: Handle `compiled.errors` from `esbuild`
+
+  return {
+    code: compiled.code,
+    frontmatter: compiled.frontmatter as FM,
+    matter: compiled.matter,
+  };
 }
