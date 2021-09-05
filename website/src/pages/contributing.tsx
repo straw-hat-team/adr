001890c550a0e59f8b63f52f07b8188115479356
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2021-09-05 01:11:08 -0400

    fix: contributing route

--- website/src/pages/contributing.tsx
+++ website/src/pages/contributing.tsx
@@ -1,8 +1,10 @@
-import { getAllFilesFrontMatter } from "@/helpers/mdx";
+import { readMdxFile } from '@/helpers/mdx';
+import { GetStaticPropsResult } from 'next';
+import { ContributingProps, Frontmatter } from '@/routes/contributing';
+
 export { Contributing as default } from '@/routes/contributing';
 
-export async function getStaticProps() {
-  const posts = await getAllFilesFrontMatter('blog');
-
-  return { props: { posts } };
+export async function getStaticProps(): Promise<GetStaticPropsResult<ContributingProps>> {
+  const post = await readMdxFile<Frontmatter>('@/routes/contributing/components/post.mdx');
+  return { props: { post } };
 }
