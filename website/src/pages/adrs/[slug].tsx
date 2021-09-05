d688cce5ed5be7fc5605a37b3efb313fba171251
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2021-09-05 03:55:58 -0400

    fix: file path

--- website/src/pages/adrs/[slug].tsx
+++ website/src/pages/adrs/[slug].tsx
@@ -1,5 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
-import { readMdxFilesOfRoute, readMdxFile, MdxFileRoute } from "@/helpers/mdx";
+import { readMdxFilesOfRoute, readMdxFile, MdxFileRoute } from '@/helpers/mdx';
 import { Slug, RouteParam, SlugProps } from '@/routes/adrs/routes/[slug]';
 import { SRC_DIR } from '@/constants';
 import { Frontmatter } from '@/routes/contributing';
@@ -7,12 +7,12 @@ import { Frontmatter } from '@/routes/contributing';
 export default Slug;
 
 export const getStaticProps: GetStaticProps<SlugProps, RouteParam> = async (props) => {
-  const post = await readMdxFile<Frontmatter>(`@/routes/adrs/[slug]/routes/${props.params!.slug}/index.mdx`);
+  const post = await readMdxFile<Frontmatter>(`@/routes/adrs/routes/[slug]/routes/${props.params!.slug}/index.mdx`);
   return { props: { post } };
 };
 
 export const getStaticPaths: GetStaticPaths<RouteParam> = async (_props) => {
-  const routes = await readMdxFilesOfRoute('@/routes/adrs/[slug]', {
+  const routes = await readMdxFilesOfRoute('@/routes/adrs/routes/[slug]', {
     atRootDir: SRC_DIR,
   });
 
--- website/src/pages/adrs/index.tsx
+++ website/src/pages/adrs/index.tsx
@@ -2,12 +2,12 @@ import { GetStaticProps } from 'next';
 import { MdxFileRoute, readMdxFilesOfRoute } from '@/helpers/mdx';
 import { SRC_DIR } from '@/constants';
 import { Adrs, AdrsProps } from '@/routes/adrs';
-import { AdrFrontmatter } from "@/routes/adrs/types";
+import { AdrFrontmatter } from '@/routes/adrs/types';
 
 export default Adrs;
 
 export const getStaticProps: GetStaticProps<AdrsProps> = async (_props) => {
-  const routes = await readMdxFilesOfRoute<AdrFrontmatter>('@/routes/adrs/[slug]', {
+  const routes = await readMdxFilesOfRoute<AdrFrontmatter>('@/routes/adrs/routes/[slug]', {
     atRootDir: SRC_DIR,
   });
 
