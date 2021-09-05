690b78dea33525bdfb1f4bd639c5ff7cd41f28ac
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2021-09-04 23:56:31 -0400

    chore: add typecheck to next.config.js

--- website/next.config.js
+++ website/next.config.js
@@ -1,19 +1,17 @@
+// @ts-check
+
 const withPlugins = require('next-compose-plugins');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
-// const remarkFrontmatter = require('remark-frontmatter');
-// const remarkToc = require('remark-toc');
 
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    // remarkPlugins: [remarkFrontmatter, remarkToc()],
-  },
-});
-
-module.exports = withPlugins([withMDX(), withBundleAnalyzer], {
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = withPlugins([withBundleAnalyzer], {
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
   pageExtensions: ['ts', 'tsx', 'mdx'],
 });
+
+module.exports = nextConfig;
