a6f98cf3b543f942bcf5b5a68e4c67d36fb02d74
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2021-09-05 00:55:46 -0400

    chore: add constants and remove dead-file

--- website/src/@types/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="@straw-hat/types/dist/css-modules" />
-
-declare module '*.mdx' {
-  export default any;
-}
-
--- website/src/constants/index.ts
+++ website/src/constants/index.ts
@@ -1,3 +1,8 @@
+import * as path from 'path';
+
 export const ORGANIZATION_NAME = 'Straw Hat Team';
 export const DISCUSSIONS_QA_URL = 'https://github.com/straw-hat-team/adr/discussions/categories/q-a';
 export const GENESIS_ADR = '0000000000';
+export const PAGE_TITLE_PREFIX = 'ADR';
+export const CWD = process.cwd();
+export const SRC_DIR = path.join(CWD, 'src');
