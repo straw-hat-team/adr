966049b328953da0cdf21c3ff3db376d7261a6a5
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2021-11-07 03:53:21 -0500

    asdas

---
+++ website/src/hooks/use-mdx-module.ts
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { getMDXModule } from '@/components/mdx';
+import type { MDXComponents } from 'mdx/types';
+
+type MdxModule<TExports> = {
+  default: React.ComponentType<{
+    components?: MDXComponents;
+  }>;
+} & TExports;
+
+export function useMdxModule<TExports = unknown>(props: { source: string; globals?: Record<string, any> }) {
+  return React.useMemo<MdxModule<TExports>>(() => getMDXModule(props.source, props.globals), [props.source]);
+}
