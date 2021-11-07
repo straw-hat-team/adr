e0055fe3a5cae986404f8c1c0436c12b209b1585
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2021-11-07 03:52:54 -0500

    asdadsa

--- website/next.config.js
+++ website/next.config.js
@@ -11,8 +11,26 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const nextConfig = {
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
-  pageExtensions: ['ts', 'tsx', 'mdx'],
+  pageExtensions: ['ts', 'tsx', 'mdx', 'md'],
   swcMinify: true,
+  experimental: {
+    esmExternals: true,
+  },
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.mdx?$/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: '@mdx-js/loader',
+          /** @type {import('@mdx-js/loader').Options} */
+          options: {},
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 module.exports = withPlugins([withBundleAnalyzer], nextConfig);
