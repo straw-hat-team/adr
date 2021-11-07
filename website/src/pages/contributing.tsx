b3f414da9226b782e3ea7bbb3b3e44d039e627dc
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2021-11-07 04:18:04 -0500

    asdas

--- website/src/pages/contributing.tsx
+++ website/src/pages/contributing.tsx
@@ -1,10 +1,11 @@
-import { readMdxFile } from '@/helpers/mdx';
+import { compileMdxFile } from '@/helpers/mdx/mdx.server';
 import { GetStaticPropsResult } from 'next';
-import { ContributingProps, Frontmatter } from '@/routes/contributing';
+import { ContributingProps } from '@/routes/contributing';
+import { Contributing } from '@/routes/contributing';
 
-export { Contributing as default } from '@/routes/contributing';
+export default Contributing;
 
 export async function getStaticProps(): Promise<GetStaticPropsResult<ContributingProps>> {
-  const post = await readMdxFile<Frontmatter>('@/routes/contributing/components/post.mdx');
-  return { props: { post } };
+  const post = await compileMdxFile('@/routes/contributing/components/post.mdx');
+  return { props: { post: post.code } };
 }
--- website/src/routes/contributing/index.tsx
+++ website/src/routes/contributing/index.tsx
@@ -1,16 +1,15 @@
-import { MDX } from '@/components/mdx';
-import { MdxFile } from '@/helpers/mdx';
-
-export type Frontmatter = {};
+import { useMdxModule } from '@/hooks/use-mdx-module';
 
 export type ContributingProps = {
-  post: MdxFile<Frontmatter>;
+  post: string;
 };
 
 export function Contributing(props: ContributingProps) {
+  const Module = useMdxModule({ source: props.post });
+
   return (
     <main className="px-4 max-w-screen-lg xl:max-w-screen-xl mx-auto space-y-10 lg:space-y-20 prose lg:prose-xl">
-      <MDX source={props.post.code} />
+      <Module.default />
     </main>
   );
 }
